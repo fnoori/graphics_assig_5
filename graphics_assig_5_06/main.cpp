acf39821e41d010a35b7465124485290b2c96fd1
Farzam Noori <farzamn80@gmail.com>
2018-04-08 15:27:17 -0600

    Initial Commit

---
+++ graphics_assig_5_06/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  graphics_assig_5_06
+//
+//  Created by Farzam Noori on 2018-04-08.
+//  Copyright © 2018 Farzam Noori. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
